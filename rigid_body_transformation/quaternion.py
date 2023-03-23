d035e525c09b74a51ccef2bf82ffa0fcdf3f3e7e
Phayuth <70325209+Phayuth@users.noreply.github.com>
2023-03-24 00:23:00 +0900

    update

--- rigid_body_transformation/quaternion.py
+++ rigid_body_transformation/quaternion.py
@@ -3,43 +3,43 @@ import numpy as np
 # quaternion = q0 + iq1 + jq2 +kq3
 # is a rotation by theta about unit vector n = (nx,ny,nz)
 
-def quaternion_from_theta_and_vector(theta,n):
-    norm_n = np.sqrt(n[0]*n[0] + n[1]*n[1] + n[2]*n[2]) # calculate norm of vector
-    nx = n[0]/norm_n # unit vector in x direction
-    ny = n[1]/norm_n # unit vector in y direction
-    nz = n[2]/norm_n # unit vector in z direction
+def quaternion_from_theta_and_vector(theta, n):
+    norm_n = np.linalg.norm(n) # calculate norm of vector
+    nx = n[0,0]/norm_n # unit vector in x direction
+    ny = n[1,0]/norm_n # unit vector in y direction
+    nz = n[2,0]/norm_n # unit vector in z direction
 
     q0 = np.cos(theta/2)
     q1 = nx*np.sin(theta/2)
     q2 = ny*np.sin(theta/2)
     q3 = nz*np.sin(theta/2)
 
-    return np.array([q0,q1,q2,q3])
+    return np.array([q0,q1,q2,q3]).reshape(4,1)
 
 def quaternion_to_theta_and_vector(q):
-    q0 = q[0]
-    q1 = q[1]
-    q2 = q[2]
-    q3 = q[3]
+    q0 = q[0,0]
+    q1 = q[1,0]
+    q2 = q[2,0]
+    q3 = q[3,0]
 
     theta = np.arccos(q0)*2
     nx = q1/np.sin(theta/2)
     ny = q2/np.sin(theta/2)
     nz = q3/np.sin(theta/2)
 
-    return theta, np.array([nx,ny,nz])
+    return theta, np.array([nx,ny,nz]).reshape(3,1)
 
 if __name__ == "__main__":
 
-    unit_vector_x = np.array([1,0,0])
-    unit_vector_y = np.array([0,1,0])
-    unit_vector_z = np.array([0,0,1])
-
+    # vector and theta
+    unit_vector_x = np.array([1,0,0]).reshape(3,1)
     theta = np.deg2rad(90)
-    print("==>> theta: ", theta)
 
-    q = quaternion_from_theta_and_vector(theta,unit_vector_x)
-    print("==>> q: ", q)
-    theta_ori,n_ori = quaternion_to_theta_and_vector(q)
-    print("==>> theta_ori: ", theta_ori)
-    print("==>> n_ori: ", n_ori)
+    # convert theta and vector to unit quaternion
+    q = quaternion_from_theta_and_vector(theta, unit_vector_x)
+    print("==>> q: \n", q)
+
+    # convert unit quaternion to theta and vector
+    theta_ori, n_ori = quaternion_to_theta_and_vector(q)
+    print("==>> theta_ori: \n", theta_ori)
+    print("==>> n_ori: \n", n_ori)
--- rigid_body_transformation/rotation_matrix.py
+++ rigid_body_transformation/rotation_matrix.py
@@ -1,12 +1,10 @@
 import numpy as np
 
-# 2d rotation
 def rotation_2d(theta):
     R = np.array([[np.cos(theta), -np.sin(theta)],
                   [np.sin(theta),  np.cos(theta)]])
     return R
 
-# 3d rotation
 def rotation_3d_x_axis(theta):
     R = np.array([[1,             0,              0],
                   [0, np.cos(theta), -np.sin(theta)],
@@ -63,6 +61,57 @@ def fixed_frame_rotation(*argv):
 
     return rot
 
+def fixed_angle_rotation(seq, angle_seq):
+    """rotation in fixed coordinate system. front multiply.
+    
+    Args:
+        seq (str): sequence of rotation. ex: xyz, zyx, xyx, ...
+        angle_seq (np.ndarray): the input of angle_seq sequence must be (gamma(x), beta(y), alpha(z))
+    """
+    if len(seq) != 3:
+        return None
+    else:
+        rotation = np.identity(3)
+        for index, value in enumerate(seq):
+            if value == 'x':
+                rotation = rotation_3d_x_axis(angle_seq[index,0]) @ rotation
+            elif value == 'y':
+                rotation = rotation_3d_y_axis(angle_seq[index,0]) @ rotation
+            elif value == 'z':
+                rotation = rotation_3d_z_axis(angle_seq[index,0]) @ rotation
+
+        return rotation
+
+def fixed_angle_rotation_to_rotation_vec(rotation_matrix):
+    pass
+
+def euler_angle_rotation(seq, angle_seq):
+    """rotation in euler coordinate system. back multiply.
+    not correct yet
+    Args:
+        seq (str): sequence of rotation. ex: xyz, zyx, xyx, ...
+        angle_seq (np.ndarray): the input of angle_seq sequence must be (gamma(x), beta(y), alpha(z))
+    """
+    if len(seq) != 3:
+        return None
+    else:
+        rotation = np.identity(3)
+        for index, value in reversed(list(enumerate(seq))): # reversed oder
+            print(index , value)
+            if value == 'x':
+                rotation = rotation_3d_x_axis(angle_seq[index,0]) @ rotation
+            elif value == 'y':
+                rotation = rotation_3d_y_axis(angle_seq[index,0]) @ rotation
+            elif value == 'z':
+                rotation = rotation_3d_z_axis(angle_seq[index,0]) @ rotation
+
+        return rotation
+
+def euler_angle_rotation_to_rotation_vec(rotation_matrix):
+    pass
+
+
+
 if __name__ == "__main__":
     np.set_printoptions(suppress=True)
 
@@ -83,10 +132,26 @@ if __name__ == "__main__":
 
     # sequence of rotation
     # for concurrent frame rotation, we post multiply of rotation matrix
-    theta = np.deg2rad(90)
-    confrot = concurrent_frame_rotation(rotation_3d_x_axis(theta), rotation_3d_y_axis(theta), rotation_3d_z_axis(theta))
-    print(confrot)
+    # theta = np.deg2rad(90)
+    # confrot = concurrent_frame_rotation(rotation_3d_x_axis(theta), rotation_3d_y_axis(theta), rotation_3d_z_axis(theta))
+    # print(confrot)
     
     # for fixed frame rotation, we pre multiply of rotation matrix
-    fixfrot = fixed_frame_rotation(rotation_3d_z_axis(theta), rotation_3d_y_axis(theta), rotation_3d_x_axis(theta))
-    print(fixfrot)
+    # fixfrot = fixed_frame_rotation(rotation_3d_z_axis(theta), rotation_3d_y_axis(theta), rotation_3d_x_axis(theta))
+    # print(fixfrot)
+
+
+    # fixed angle rotation
+    gamma = np.pi/2
+    beta = np.pi/2
+    alpha = np.pi/4
+    gamma_beta_alpha = np.array([gamma,beta,alpha]).reshape(3,1)
+
+    # fixed_angle_rot = fixed_angle_rotation('xyz', gamma_beta_alpha)
+    # print("==>> fixed_angle_rot: \n", fixed_angle_rot)
+
+    # fixed_angle_rot_check = rotation_3d_z_axis(alpha) @ rotation_3d_y_axis(beta) @ rotation_3d_x_axis(gamma)
+    # print("==>> fixed_angle_rot_check: \n", fixed_angle_rot_check)
+
+    euler_angle_rot = euler_angle_rotation('zyx', gamma_beta_alpha)
+    print("==>> euler_angle_rot: \n", euler_angle_rot)
