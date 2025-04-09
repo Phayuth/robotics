901c3df65992e7d5121af9460c8563a49eed932e
yuth <yuth@todo.com>
2025-04-09 10:48:28 +0900

    update

--- projects/paper_torus/fig_cspace.py
+++ projects/paper_torus/fig_cspace.py
@@ -6,26 +6,48 @@ sys.path.append(str(os.path.abspath(os.getcwd())))
 import matplotlib.pyplot as plt
 from simulator.sim_planar_rr import RobotArm2DSimulator
 import numpy as np
-from task_map import PaperICCAS2024, PaperTorusIFAC2025
+from task_map import PaperICCAS2024, PaperTorusIFAC2025, PaperX202X
 
-# cspace
-torus = False
-env = RobotArm2DSimulator(PaperICCAS2024(), torusspace=True)
-fig, ax = plt.subplots(1, 1)
-colp = env.plot_cspace(ax)
-if torus:
-    ax.set_xlim([-2 * np.pi, 2 * np.pi])
-    ax.set_ylim([-2 * np.pi, 2 * np.pi])
-    ax.axhline(y=np.pi, color="gray", alpha=0.4)
-    ax.axhline(y=-np.pi, color="gray", alpha=0.4)
-    ax.axvline(x=np.pi, color="gray", alpha=0.4)
-    ax.axvline(x=-np.pi, color="gray", alpha=0.4)
-else:
-    ax.set_xlim([-np.pi, np.pi])
-    ax.set_ylim([-np.pi, np.pi])
-plt.show()
 
-if torus:
-    np.save("./datasave/planner_ijcas_data/collisionpoint_so2s.npy", colp)
-else:
-    np.save("./datasave/planner_ijcas_data/collisionpoint_exts.npy", colp)
+def fig_cspace_2d():
+    """
+    generate cspace figure with so2(minimal space), extended space.
+    switch between torus=true/false for the corresponding
+    """
+    torus = False
+    env = RobotArm2DSimulator(PaperICCAS2024(), torusspace=True)
+    fig, ax = plt.subplots(1, 1)
+    colp = env.plot_cspace(ax)
+    if torus:
+        ax.set_xlim([-2 * np.pi, 2 * np.pi])
+        ax.set_ylim([-2 * np.pi, 2 * np.pi])
+        ax.axhline(y=np.pi, color="gray", alpha=0.4)
+        ax.axhline(y=-np.pi, color="gray", alpha=0.4)
+        ax.axvline(x=np.pi, color="gray", alpha=0.4)
+        ax.axvline(x=-np.pi, color="gray", alpha=0.4)
+    else:
+        ax.set_xlim([-np.pi, np.pi])
+        ax.set_ylim([-np.pi, np.pi])
+    plt.show()
+
+    if torus:
+        np.save("./datasave/planner_ijcas_data/collisionpoint_so2s.npy", colp)
+    else:
+        np.save("./datasave/planner_ijcas_data/collisionpoint_exts.npy", colp)
+
+
+def fig_sequential_task_2d():
+    """
+    generate and visualize figure with minimal and extended space.
+    """
+    torus = False
+    env = RobotArm2DSimulator(PaperX202X(), torusspace=torus)
+    task0 = np.array([-np.pi / 2, 0.0])  # initial configuration
+    task1 = np.array([-1.0, 2.0])  # grab cup
+    task2 = np.array([np.pi + 1, -2.0])  # place cup
+    thetas = np.vstack((task0, task1, task2)).T
+    env.plot_view(thetas)
+
+if __name__=="__main__":
+    # fig_cspace_2d()
+    fig_sequential_task_2d()
